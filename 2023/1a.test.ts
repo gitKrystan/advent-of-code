ff9982c8457fbf798dcc591b913906ff317d885a
Krystan HuffMenne <kmenne+github@gmail.com>
2023-12-01 20:57:42 -0800

    Hello World

---
+++ 2023/1a.test.ts
@@ -0,0 +1,5 @@
+import { describe, test } from '@jest/globals';
+
+describe('hello world', () => {
+  test.todo('hello world');
+});
---
+++ jest.config.cjs
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+const config = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+};
+
+module.exports = config;
